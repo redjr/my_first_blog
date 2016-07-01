e823479647b42d33c8fbf4cfe619fd5dfd32dbcd
redjr <raymundoesjr@yahoo.com.br>
2016-07-01 19:45:50 -0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+# criando o método def chamado post_list,
+# que aceita o pedido(request) e retorna um
+# método render que será processado (para montar)
+# nosso modelo blog/post_list.html
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
