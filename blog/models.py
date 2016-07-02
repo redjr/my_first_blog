0ea49e0dc1c5239aae36a4917edf40a17a41245c
redjr <raymundoesjr@yahoo.com.br>
2016-07-02 12:08:04 -0300

    Changed the HTML for the site.

--- blog/models.py
+++ blog/models.py
@@ -12,16 +12,15 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-                    default = timezone.now)
-    publish_date = models.DateTimeField(
-                    blank=True, null=True)
+    created_date = models.DateTimeField(auto_now=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    publish_date = models.DateTimeField(blank=True, null=True)
 
 
-    def publish(self):
-        self.publish_date = timezone.now()
-        self.save()
+def publish(self):
+    self.publish_date = timezone.now()
+    self.save()
 
 
-    def __str__(self):
-        return self.title
+def __str__(self):
+    return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from . models import Post
 # Create your views here.
+# uma completa aqui: http://stackoverflow.com/questions/35377179/django-noreversematch-error-on-production
+# outra completa aqui: http://stackoverflow.com/questions/31228794/noreversematch-error-finding-it-hard
 # criando o método def chamado post_list,
 # que aceita o pedido(request) e retorna um
 # método render que será processado (para montar)
 # nosso modelo blog/post_list.html
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    #exibindo posts no blog, por ordem de publicação por date
+    # published_date_lte fica dando erro: Cannot resolve keyword 'published_date' into field
+    # porém, publish_date__lte tudo funciona corretamente
+    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    return render(request, 'blog/post_list.html', { 'posts' : posts })
